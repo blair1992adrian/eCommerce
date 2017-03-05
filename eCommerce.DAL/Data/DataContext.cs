4ea16232538907a1d7cc7a6b3b38603d262e16fb
Adrian Blair <adrianblair1992@gmail.com>
2017-03-05 13:47:41 +0000

    Began Setting up the Data Access Layer using entity framework and the repository pattern.

---
+++ eCommerce.DAL/Data/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using eCommerce.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.DAL.Data
+{
+
+    public class DataContext : DbContext
+    {
+        /// <summary>
+        /// you can pass the NAME of a connection string (e.g. stored in web.config).
+        /// </summary>
+        public DataContext()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
